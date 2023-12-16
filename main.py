43fa52ada1250c47852530118e3cca2f331909d2
Goodylaptop <vivitthachai@hotmail.com>
2023-12-16 16:01:55 +0700

    deleted comment in main.py line 4

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import demographic_data_analyzer
 import test_module
 from unittest import TestProgram
 
-# check and test mofules
 print(demographic_data_analyzer.calculate_demographic_data_analyzer(print_test=False))
 #line for unit testing
 TestProgram(module='test_module',exit=False)
