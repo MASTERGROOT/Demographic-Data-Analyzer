a2090f0c25fff0c53a14e9350e4ee4cb4540d440
Goodylaptop <vivitthachai@hotmail.com>
2023-12-16 15:01:25 +0700

    added comment in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import demographic_data_analyzer
 import test_module
 from unittest import TestProgram
 
-
+# check and test mofules
 print(demographic_data_analyzer.calculate_demographic_data_analyzer(print_test=False))
 
 TestProgram(module='test_module',exit=False)
